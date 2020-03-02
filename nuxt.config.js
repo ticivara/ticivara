7a9c95c1af5229739a1c86ee5655f787bca6d565
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2020-03-02 09:33:34 +0000

    first

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+export default {
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/main.sass'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    /*
+      NOTE: StyleLint autofixing conflicts with eslint and they keep changing the
+      same file. For example fighting over the last newline in a <style lang="sass"> block.
+    */
+    '@nuxtjs/stylelint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    // https://github.com/nuxt-community/style-resources-module
+    '@nuxtjs/style-resources'
+  ],
+  styleResources: {
+    sass: ['./assets/_colors.sass', './assets/_mixins.sass']
+  },
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ pages/sabong.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Sabong</h1>
+    <p class="intro">Sabong sewing</p>
+  </div>
+</template>
+
+<style lang="sass">
+.intro
+  font-style: italic
+  text-align: center
+</style>
