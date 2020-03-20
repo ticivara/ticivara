2aefeb54ea3142b62e8368a0b31f09b01451f334
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2020-03-20 10:02:47 +0000

    thai

--- components/LandingHero.vue
+++ components/LandingHero.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="landing-hero" class="hero is-medium">
-    <div class="hero-body">
+    <div v-if="lang === 'english'" class="hero-body">
       <div class="welcome-message">
         <p>
           <strong><em>ti-civara:</em> three robes</strong>
@@ -15,5 +15,46 @@
         <img src="/img/logo-as-path.svg" />
       </div>
     </div>
+
+    <div v-if="lang === 'thai'" class="hero-body">
+      <div class="welcome-message">
+        <p>
+          <strong>ไตรจีวร</strong>
+        </p>
+        <p>
+          คือผ้า 3 ผืนที่เป็นหนึ่งในบริขาร 8 อย่าง "อัฏฐบริขาร" ที่พระภิกษุจำเป็นต้องมี
+          ประกอบด้วยผ้าสามผืน อันได้แก่ ผ้าสบง "อันตรวาสก" ผ้าจีวร "อุตตราสงค์" และผ้าห่มซ้อน
+          "สังฆาฏิ"
+        </p>
+        <p>
+          ในสมัยต้นพุทธกาลผ้าที่นำมาทำเป็นไตรจีวรยังมีการใช้ผ้าทั้งผืน
+          หรือนำเก็บผ้ามาต่อกันอย่างไม่มีรูปแบบ
+          เมื่อมีการลักขโมยผ้าของพระบ่อยครั้งเนื่องจากผ้าในสมัยนั้นหายาก
+          พระพุทธองค์จึงโปรดให้พระอานนท์ออกแบบทำผ้าเป็นลายคันนาเพื่อทำลายราคาของผ้า
+          จึงเกิดเป็นจีวรขันธ์สืบทอดมาถึงทุกวันนี้
+        </p>
+        <p>
+          การตัดเย็บจีวรเป็นภูมิปัญญาจากวัด ซึ่งสืบทอดกันต่อๆ
+          มาโดยเฉพาะพระสายวัดป่าจะฝึกให้สามเณรตัดเย็บไตรจีวรของตนเองเพื่อใช้ในการบวช
+        </p>
+        <p>
+          การคำนวณที่ใช้ในเว็บนี้เป็นเพียงหนึ่งของวิธีที่หลากหลายในการตัดเย็บ
+          หากมีข้อสงสัยสามารถสอบถามได้ที่ ratanawanno@gmail.com
+        </p>
+      </div>
+      <div class="landing-logo">
+        <img src="/img/logo-as-path.svg" />
+      </div>
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    lang() {
+      return this.$store.state.lang;
+    }
+  }
+};
+</script>
--- pages/th/civara-pattern.vue
+++ pages/th/civara-pattern.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>
-    <h1>Civara Guide</h1>
+  <div class="pattern-container container">
+    <div>
+      <div class="content">
+        <h1 class="page-title">{{ title }}</h1>
+        <div class="page-content">
+          <CivaraPattern />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CivaraPattern from '@/components/CivaraPattern.vue';
+
 export default {
+  components: {
+    CivaraPattern
+  },
+
   fetch(context) {
     context.store.commit('setLangFromPath', context.route.path);
+  },
+
+  asyncData() {
+    return {
+      title: 'Civara Pattern'
+    };
+  },
+
+  head() {
+    return {
+      title: this.title.trim() + ' - Ticivara Robe Sewing'
+    };
   }
 };
 </script>
